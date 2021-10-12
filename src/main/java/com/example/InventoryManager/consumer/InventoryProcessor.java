9031add7e86eba4581f91ea11c362eeb153b319f
Muhib_ur_Rehman <mrehman@nisum.com>
2021-10-12 13:35:34 +0500

    Exception handled of invalid item_id

--- src/main/java/com/example/InventoryManager/consumer/InventoryProcessor.java
+++ src/main/java/com/example/InventoryManager/consumer/InventoryProcessor.java
@@ -9,6 +9,8 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.NoSuchElementException;
+
 @Component
 public class InventoryProcessor {
 
@@ -21,17 +23,23 @@ public class InventoryProcessor {
     @RabbitListener(queues = InventoryConfig.INVENTORY_QUEUE)
     public void consumeMessageFromQueue(OrderInfo order){
         System.out.println("Message received from queue : " + order);
-        if (order.getPaymentStatus().equals("ACCEPTED")){
-            Inventory orderedItemInfo = this.inventoryService.getInventoryItem(order.getItemId()).get();
-            if (order.getQty() > orderedItemInfo.getQty()){
-                order.setOrderStatus("CANCELLED");
-                template.convertAndSend(InventoryConfig.EXCHANGE,InventoryConfig.ROUTING_KEY,order);
+        try {
+            if (order.getPaymentStatus().equals("ACCEPTED")){
+                Inventory orderedItemInfo = this.inventoryService.getInventoryItem(order.getItemId()).get();
+                if (order.getQty() > orderedItemInfo.getQty()){
+                    order.setOrderStatus("CANCELLED");
+                    template.convertAndSend(InventoryConfig.EXCHANGE,InventoryConfig.ROUTING_KEY,order);
+                }
+                else if (order.getQty() <= orderedItemInfo.getQty()){
+                    order.setOrderStatus("IN-PROCESS");
+                    template.convertAndSend(InventoryConfig.EXCHANGE,InventoryConfig.ROUTING_KEY,order);
+                    this.inventoryService.updateInventory(order.getItemId() ,orderedItemInfo.getQty() - order.getQty());
+                }
             }
-            else if (order.getQty() <= orderedItemInfo.getQty()){
-                order.setOrderStatus("IN-PROCESS");
-                template.convertAndSend(InventoryConfig.EXCHANGE,InventoryConfig.ROUTING_KEY,order);
-                this.inventoryService.updateInventory(order.getItemId() ,orderedItemInfo.getQty() - order.getQty());
-            }
+        }
+        catch (NoSuchElementException e){
+            order.setOrderStatus("CANCELLED");
+            template.convertAndSend(InventoryConfig.EXCHANGE,InventoryConfig.ROUTING_KEY,order);
         }
     }
 }
